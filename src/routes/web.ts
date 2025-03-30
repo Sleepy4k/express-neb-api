ca7778b3a114c14b848d144dd7f1b75edab136d7
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-03-30 13:36:17 +0700

    chore(route): update web route

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,22 +1,28 @@
-import * as ac from "@controllers/web/admin.controller.js";
-import * as hc from "@controllers/web/home.controller.js";
-import * as sc from "@controllers/web/service.controller.js";
-import * as tc from "@controllers/web/tutorial.controller.js";
+import * as loginController from "@controllers/web/auth/login.controller.js";
+import * as generateController from "@controllers/web/dashboard/generate.controller.js";
+import * as homeController from "@controllers/web/landing/home.controller.js";
+import * as serviceController from "@controllers/web/landing/service.controller.js";
+import * as tutorialController from "@controllers/web/landing/tutorial.controller.js";
+import * as sebController from "@controllers/web/service/seb.controller.js";
+import authenticationHandler from "@middleware/authentication.js";
 import fileUploader from "@middleware/fileUploader.js";
 import { Router } from "express";
 
 const webRouter = Router();
 
-webRouter.get("/", hc.home);
+webRouter.get("/", homeController.home);
 
-webRouter.get("/service", sc.form);
-webRouter.post("/service", sc.missUrl);
-webRouter.post("/service/:redeemCode", fileUploader.single("file"), sc.bypass);
+webRouter.get("/login", loginController.form);
+webRouter.post("/login", loginController.process);
 
-webRouter.get("/tutorial", tc.home);
+webRouter.get("/service", serviceController.home);
 
-webRouter.get("/dashboard", ac.home);
-webRouter.post("/dashboard/generate", ac.registerRedeemCode);
-webRouter.get("/dashboard/list/:name", ac.findListRedeemCode);
+webRouter.get("/service/seb", sebController.form);
+webRouter.post("/service/seb", sebController.missUrl);
+webRouter.post("/service/seb/:redeemCode", fileUploader.single("file"), sebController.bypass);
+
+webRouter.get("/tutorial", tutorialController.home);
+
+webRouter.get("/dashboard", authenticationHandler, generateController.home);
 
 export default webRouter;
