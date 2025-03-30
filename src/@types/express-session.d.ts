218d79c94ed47f18ac949e08129996f833955f08
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-03-30 13:39:48 +0700

    feat(type): add express session type

---
+++ src/@types/express-session.d.ts
@@ -0,0 +1,11 @@
+import "express-session";
+
+declare module "express-session" {
+  interface SessionData {
+    user?: {
+      email: string;
+      password: string;
+      role: string;
+    };
+  }
+}
