adf2a7efdd4853f27582ff33f00f13b8a6df687a
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-03-31 15:53:00 +0700

    feat(config): add asset config and update limiter and minify config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,9 @@
 import { appConfig } from "./app.config.js";
+import { assetConfig } from "./asset.config.js";
 import { cspConfig } from "./csp.config.js";
 import { rateLimitConfig } from "./limiter.config.js";
 import { minifyConfig } from "./minify.config.js";
 import { serviceConfig } from "./service.config.js";
 import { sessionConfig } from "./session.config.js";
 
-export { appConfig, cspConfig, minifyConfig, rateLimitConfig, serviceConfig, sessionConfig };
+export { appConfig, assetConfig, cspConfig, minifyConfig, rateLimitConfig, serviceConfig, sessionConfig };
