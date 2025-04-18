f6f2baa94b677dff6aa90ce4ab2d2a33fef242f4
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-04-18 17:32:42 +0700

    chore(route): update api route

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,8 +1,12 @@
+import * as datatable from "@controllers/api/datatable.controller.js";
 import * as homeController from "@controllers/api/home.controller.js";
 import { Router } from "express";
 
 const apiRouter = Router();
 
 apiRouter.get("/", homeController.home);
+
+apiRouter.get("/datatable/config", datatable.configuration);
+apiRouter.get("/datatable/localisation", datatable.localisation);
 
 export default apiRouter;
