90d44c6a0de52b82688a57065a4738e538ae8e17
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-04-28 02:05:40 +0700

    chore(config): change web3form env key due typo

--- src/config/web3form.config.ts
+++ src/config/web3form.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable perfectionist/sort-objects */
 export const web3formConfig = {
-  form_url: process.env.WEB3FORM_FORM_URL ?? "https://api.web3forms.com/submit",
-  access_key: process.env.WEB3FORM_ACCESS_KEY ?? "",
+  form_url: process.env.WEB3FORMS_FORM_URL ?? "https://api.web3forms.com/submit",
+  access_key: process.env.WEB3FORMS_ACCESS_KEY ?? "",
 };
