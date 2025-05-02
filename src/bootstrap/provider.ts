d189fe842452a3b562b8c8731088006f73cdd955
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-05-02 20:32:48 +0700

    chore(bootstrap): update app bootstrap

--- src/bootstrap/provider.ts
+++ src/bootstrap/provider.ts
@@ -1,6 +1,15 @@
 import appServiceProvider from "@providers/app.provider.js";
+import csrfServiceProvider from "@providers/csrf.provider.js";
 import { type Express } from "express";
 
 export default (app: Express): void => {
+  /**
+   * Register the application service provider
+   */
   app.use(appServiceProvider);
+
+  /**
+   * Register the CSRF service provider
+   */
+  app.use(csrfServiceProvider);
 };
