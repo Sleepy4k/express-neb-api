2175633a9af73fdc9179deed3c3ad9dd0e0d37e9
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-05-07 01:04:56 +0700

    chore(route): update api route

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,7 +4,10 @@ import { Router } from "express";
 // Controllers
 import * as datatableController from "@controllers/api/datatable.controller.js";
 import * as homeController from "@controllers/api/home.controller.js";
+import * as teleStatusController from "@controllers/api/telegram/status.controller.js";
 import trakteerController from "@controllers/api/webhook/trakteer.controller.js";
+import saweriaController from "@controllers/api/webhook/saweria.controller.js";
+import telegramApi from "@middleware/telegramApi.js";
 
 const apiRouter = Router();
 
@@ -15,7 +18,11 @@ apiRouter.get("/", homeController.home);
 apiRouter.get("/datatable/config", datatableController.configuration);
 apiRouter.get("/datatable/localisation", datatableController.localisation);
 
+// Telegram
+apiRouter.get("/telegram/status", telegramApi, teleStatusController.home);
+
 // Webhook
 apiRouter.post("/webhook/trakteer", trakteerController);
+apiRouter.post("/webhook/saweria", saweriaController);
 
 export default apiRouter;
