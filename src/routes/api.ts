4418fb56214e9700ccd3820e78723a4cae2dd24a
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2025-05-14 02:17:57 +0700

    chore(route): update api route

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -7,10 +7,14 @@ import * as homeController from "@controllers/api/home.controller.js";
 import * as teleStatusController from "@controllers/api/telegram/status.controller.js";
 import * as telePricingController from "@controllers/api/telegram/pricing.controller.js";
 import * as teleFAQController from "@controllers/api/telegram/faq.controller.js";
-import * as teleUsersController from "@controllers/api/telegram/users.controller.js";
+import * as teleUsersController from "../app/controllers/api/telegram/users.controller.js";
+import * as teleServiceController from "../app/controllers/api/telegram/service.controller.js";
+import * as teleTokenController from "../app/controllers/api/telegram/token.controller.js";
+import * as teleSebController from "../app/controllers/api/telegram/seb.controller.js";
 import trakteerController from "@controllers/api/webhook/trakteer.controller.js";
 import saweriaController from "@controllers/api/webhook/saweria.controller.js";
 import telegramApi from "@middleware/telegramApi.js";
+import { sebFileUploader } from "../app/middleware/fileUploader.js";
 
 const apiRouter = Router();
 
@@ -26,6 +30,10 @@ apiRouter.get("/telegram/status", telegramApi, teleStatusController.home);
 apiRouter.get("/telegram/pricing", telegramApi, telePricingController.home);
 apiRouter.get("/telegram/faq", telegramApi, teleFAQController.home);
 apiRouter.get("/telegram/users", telegramApi, teleUsersController.home);
+apiRouter.get("/telegram/service", telegramApi, teleServiceController.home);
+apiRouter.get("/telegram/service/check/:redeemCode", telegramApi, teleTokenController.check);
+apiRouter.post("/telegram/service/seb", telegramApi, teleSebController.missUrl);
+apiRouter.post("/telegram/service/seb/:redeemCode", telegramApi, sebFileUploader.single("file"), teleSebController.bypass);
 
 // Webhook
 apiRouter.post("/webhook/trakteer", trakteerController);
